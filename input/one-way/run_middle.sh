9fbc36b27902c54d3dfaa5409f1ef2a9ba983b85
Fabian Kutschera <82662056+fabian-kutschera@users.noreply.github.com>
2023-06-09 20:38:55 +0200

    Update and rename runconverter_NORTH_ICELAND.sh to input/one-way/run_middle.sh

--- input/one-way/run_middle.sh
+++ input/one-way/run_middle.sh
@@ -1,13 +1,8 @@
-#Fabian Kutschera 20.12.2020
-
 module load intel/2019 mpi.intel/2020
 export Mylibs=/import/freenas-m-05-seissol/SamoaRelated/myLibs
 export HDF5_BASE=/import/freenas-m-05-seissol/SamoaRelated/samoa/build/intel/parallel/
 export LD_LIBRARY_PATH=$NETCDF_BASE/lib/:$HDF5_BASE/lib/:$Mylibs/installDir/lib:$LD_LIBRARY_PATH
 
-#INPUT
-#wrong format because .h5 instead of posix (.bin) --> conversion first with conversion_h5_to_bin.sh 
-#input=/import/freenas-m-05-seissol/bo/CHEESE/forFabian/middle_M7.333/HFFtest-surface.xdmf
 
 #DR West:
 #input=/import/freenas-m-05-seissol/kutschera/HIWI/SeisSol/surface_output_Iceland/west_M7.343_simple/west_M7.343/HFFtest_resampled-surface.xdmf 
@@ -32,7 +27,6 @@ output=tanioka_complex_middle_M7.07.nc
 
 #BATHYMETRY
 bathy=/import/freenas-m-05-seissol/kutschera/HIWI/BathymetryNorthIceland.grd
-#bathy=/import/freenas-m-05-seissol/kutschera/HIWI/BathymetryNorthIceland_test.grd
 
 
 export HDF5_BASE=/import/freenas-m-05-seissol/SamoaRelated/samoa/build/intel/parallel/
